5208138085ff792565b19a3285d3d6afb540a8d8
Qinyun Wang <oojbjoo@gmail.com>
2020-06-19 19:48:36 +0800

    Solution1 add appContext to controller

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,34 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * It contradicts the principle of inversion of control,
+ * as we request the dependencies from the container directly.
+ */
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingService getGreetingService() {
+        return applicationContext.getBean(GreetingService.class);
     }
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = getGreetingService();
+        System.out.println("Greeting Service: " + greetingService);
+        System.out.println("Greeting Controller: " + this + "\n");
         return greetingService.sayHi();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }

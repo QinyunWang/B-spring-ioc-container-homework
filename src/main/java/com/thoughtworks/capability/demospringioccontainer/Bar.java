49c10c758a6998575936f2c47d53498bb625de42
Qinyun Wang <oojbjoo@gmail.com>
2020-06-19 22:59:27 +0800

    Solution3 use annotation PostConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,11 +7,6 @@ public class Bar {
 
     private Foo foo;
 
-    /**
-     * This way Spring creates the beans, but the dependencies are not injected until they are needed.
-     * @param foo
-     */
-    @Autowired
     public void setFoo(Foo foo) {
         this.foo = foo;
     }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,20 +1,26 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
 
     private Bar bar;
 
-    /**
-     * This way Spring creates the beans, but the dependencies are not injected until they are needed.
-     * @param bar
-     */
     @Autowired
-    public void setBar(Bar bar) {
+    public Foo(Bar bar) {
         this.bar = bar;
+    }
+
+    /**
+     * Another way to break the cycle is injecting a dependency using @Autowired on one of the beans, and then use a
+     * method annotated with @PostConstruct to set the other dependency.
+     */
+    @PostConstruct
+    public void init() {
+        bar.setFoo(this);
     }
 }

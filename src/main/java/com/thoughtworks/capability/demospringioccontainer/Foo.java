7687d1cc16ca4f708de690bc5edfd364454fd4b9
Qinyun Wang <oojbjoo@gmail.com>
2020-06-19 22:44:47 +0800

    Solution1 use annotation lazy

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +8,13 @@ public class Foo {
 
     private Bar bar;
 
-    public Foo(Bar bar) {
+    /**
+     * A simple way to break the cycle is saying Spring to initialize one of the beans lazily. That is: instead of fully
+     * initializing the bean, it will create a proxy to inject it into the other bean. The injected bean will only be
+     * fully created when it’s first needed.
+     * @param bar
+     */
+    public Foo(@Lazy Bar bar) {
         this.bar = bar;
     }
 }
